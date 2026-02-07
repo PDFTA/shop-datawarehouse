b9814ed445273ad4805bceda006099732c971e40
John Carney <jaspajjr@users.noreply.github.com>
2026-02-07 23:29:35 +0000

    Seems to be working locally

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -1,8 +1,9 @@
 # Cloud Run service
 resource "google_cloud_run_v2_service" "shop_datawarehouse" {
-  name     = "shop-datawarehouse"
-  location = var.cloud_run_region
-  project  = var.gcp_project_id
+  name               = "shop-datawarehouse"
+  location           = var.cloud_run_region
+  project            = var.gcp_project_id
+  deletion_protection = false
 
   template {
     service_account = google_service_account.cloud_run_sa.email
--- terraform/workload_identity.tf
+++ terraform/workload_identity.tf
@@ -1,9 +1,8 @@
 # Workload Identity Pool for GitHub Actions
 resource "google_iam_workload_identity_pool" "github_actions_pool" {
   project                   = var.gcp_project_id
-  workload_identity_pool_id = "github-actions-pool"
+  workload_identity_pool_id = "github-pool"
   display_name              = "GitHub Actions Pool"
-  description               = "Workload Identity Pool for GitHub Actions"
 
   depends_on = [
     google_project_service.iam_api,
@@ -17,8 +16,6 @@ resource "google_iam_workload_identity_pool_provider" "github_actions_provider" 
   project                            = var.gcp_project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.github_actions_pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "github-actions-provider"
-  display_name                       = "GitHub Actions Provider"
-  description                        = "Workload Identity Pool Provider for GitHub Actions"
 
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
@@ -30,15 +27,5 @@ resource "google_iam_workload_identity_pool_provider" "github_actions_provider" 
     "attribute.repository" = "assertion.repository"
   }
 
-  # Optional: Add repository condition to restrict access
-  attribute_condition = var.github_repository != "" ? "attribute.repository==\"${var.github_repository}\"" : null
-}
-
-# Bind the GitHub Actions service account to the workload identity pool
-resource "google_service_account_iam_member" "github_actions_workload_identity_binding" {
-  service_account_id = google_service_account.github_actions_sa.name
-  role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_actions_pool.name}/attribute.repository/${var.github_repository}"
-
-  depends_on = [google_iam_workload_identity_pool_provider.github_actions_provider]
+  attribute_condition = "assertion.repository_owner_id != ''"
 }
