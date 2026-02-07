773d1b2a4944116adfdb2eb0f774fa0d44541316
John Carney <jaspajjr@users.noreply.github.com>
2026-02-07 21:17:46 +0000

    Unlocked important APIs

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,4 +1,7 @@
 # Enable required GCP APIs
+# These APIs must be enabled before other resources can be created
+
+# Cloud Run - Required for deploying the application
 resource "google_project_service" "cloud_run_api" {
   project = var.gcp_project_id
   service = "run.googleapis.com"
@@ -6,6 +9,7 @@ resource "google_project_service" "cloud_run_api" {
   disable_on_destroy = false
 }
 
+# Artifact Registry - Required for storing Docker images
 resource "google_project_service" "artifact_registry_api" {
   project = var.gcp_project_id
   service = "artifactregistry.googleapis.com"
@@ -13,9 +17,34 @@ resource "google_project_service" "artifact_registry_api" {
   disable_on_destroy = false
 }
 
+# IAM - Required for managing service accounts and permissions
 resource "google_project_service" "iam_api" {
   project = var.gcp_project_id
   service = "iam.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+# IAM Credentials - Required for Workload Identity Federation
+resource "google_project_service" "iam_credentials_api" {
+  project = var.gcp_project_id
+  service = "iamcredentials.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+# Cloud Resource Manager - Required for project-level IAM bindings
+resource "google_project_service" "cloud_resource_manager_api" {
+  project = var.gcp_project_id
+  service = "cloudresourcemanager.googleapis.com"
+
+  disable_on_destroy = false
+}
+
+# Security Token Service - Required for Workload Identity Federation
+resource "google_project_service" "sts_api" {
+  project = var.gcp_project_id
+  service = "sts.googleapis.com"
 
   disable_on_destroy = false
 }
